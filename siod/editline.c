55bdddcca80906d63090872309c0a7838bf44f44
Alan W Black <awb@cs.cmu.edu>
2020-07-10 09:53:40 -0400

    fix warnings

--- siod/editline.c
+++ siod/editline.c
@@ -73,7 +73,7 @@
 /* modified by awb to allow specifcation of history size at run time  */
 /* (though only once)                                                 */
 int editline_histsize=256;
-char *editline_history_file;
+char *editline_history_file = ".editline_history";
 /* If this is defined it'll be called for completion first, before the */
 /* internal file name completion will be                               */
 EL_USER_COMPLETION_FUNCTION_TYPE*el_user_completion_function = NULL;
