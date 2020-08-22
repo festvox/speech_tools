20a8e339599837b3de36508bcd0681bef583cd15
Sergio Oller <sergioller@gmail.com>
2020-08-22 09:45:43 +0200

    DOC++ to doxygen for EST_Contents.h

--- include/EST_Contents.h
+++ include/EST_Contents.h
@@ -39,7 +39,8 @@
 #ifndef __EST_CONTENTS_H__
 #define __EST_CONTENTS_H__
 
-/** A class for containing some other (arbitrary) class    
+/** \class EST_Content_Data
+    \brief A class for containing some other (arbitrary) class.
  Not general enough to call itself a run-time type system  
  Is designed to solve the problem of holding user          
  specified information.
